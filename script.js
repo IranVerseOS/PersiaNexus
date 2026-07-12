a85f57c3f7f8b2ab7f7c7becbbd26d449ef0005d
IranVerseOS <iranversestudio@gmail.com>
2026-07-12 20:38:31 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 console.log("IranVerse OS Initialized");
 console.log(IranVerseHeroes);
+const aiButton = document.getElementById("ai-check");
+
+const aiStatus = document.getElementById("ai-status");
+
+
+if(aiButton){
+
+aiButton.onclick = function(){
+
+if(window.IranVerseAI){
+
+aiStatus.innerHTML = 
+`
+Online ✅ <br>
+Mode: ${IranVerseAI.mode}<br>
+Version: ${IranVerseAI.version}
+`;
+
+}
+
+else{
+
+aiStatus.innerHTML =
+"AI Core Not Connected";
+
+}
+
+};
+
+}
