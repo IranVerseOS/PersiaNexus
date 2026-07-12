9fdce1fa24333ef5856f3312f8021f62e511233e
IranVerseOS <iranversestudio@gmail.com>
2026-07-12 15:33:12 +0330

    Update heroes-data.js

--- core/core/heroes-data.js
+++ core/core/heroes-data.js
@@ -1,1 +1,21 @@
+const IranVerseHeroes = [
 
+    {
+        name: "Arvand",
+
+        title: "The First Guardian of IranVerse",
+
+        role: "Guardian",
+
+        powers: [
+            "Energy Control",
+            "Ancient Wisdom",
+            "Guardian Spirit"
+        ],
+
+        origin: "IranVerse Universe",
+
+        status: "Active"
+    }
+
+];
