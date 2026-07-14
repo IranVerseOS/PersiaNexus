24f861dbd33b4c016e76aa6666f1b571a87a8391
IranVerseOS <iranversestudio@gmail.com>
2026-07-14 10:05:08 +0330

    Update nft.js

--- nft/nft.js
+++ nft/nft.js
@@ -1,1 +1,31 @@
+async function loadHeroes() {
 
+    const response = await fetch("../characters/heroes.json");
+
+    const data = await response.json();
+
+    const gallery = document.getElementById("gallery");
+
+    gallery.innerHTML = "";
+
+    data.heroes.forEach(hero => {
+
+        gallery.innerHTML += `
+
+        <div class="hero-card">
+
+            <h3>${hero.name}</h3>
+
+            <p>${hero.title}</p>
+
+            <span>${hero.origin}</span>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+loadHeroes();
